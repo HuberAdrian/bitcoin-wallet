89c35edb39c4945c7781d35b13946af7661d719a
HuberAdrian <huberadrian@web.de>
2022-09-08 21:55:42 +0200

    added browserify plugin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 
 
 
 
 
 function App() {
+
+
     const bip39 = require('bip39')
     const bitcoin = require('bitcoinjs-lib')
     const { BIP32Interface } = require('bip32')
@@ -16,6 +19,7 @@ function App() {
     const network = bitcoin.networks.bitcoin //use networks.testnet for testnet
 
     const bip32 = BIP32Factory(ecc)
+
 
     // Derivation path
     const path = `m/44'/0'/0'/0`
